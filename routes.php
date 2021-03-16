52d4e12c42acfcb8a7d11f5d289e2a9b7e0b7bd4
phprouter <80062791+phprouter@users.noreply.github.com>
2021-03-16 16:25:07 +0100

    Update routes.php
    updated the routes with callbacks

--- routes.php
+++ routes.php
@@ -2,41 +2,79 @@
 
 require_once(__DIR__.'/router.php');
 
-// ##############################
+// ##################################################
+// ##################################################
+// ##################################################
+
+// Static GET
+// In the URL -> http://localhost
+// The output -> Index
 get('/', function(){
-  require_once(__DIR__.'/views/view_index.php');
+  echo 'Index';
   exit();
 });
 
-// ##############################
-get('/routing', function(){
-  require_once(__DIR__.'/views/view_routing.php');
+// Dynamic GET. Example with 1 variable
+// In the URL -> http://localhost/user/111
+// The output -> The id passed is: 111
+get('/user/:id', function($id){
+  echo "The id passed is: $id ";
   exit();
 });
 
-// ##############################
-get('/csrf', function(){
-  require_once(__DIR__.'/views/view_csrf.php');
+// Dynamic GET. Example with 2 variables
+// In the URL -> http://localhost/user/A/B
+// The output -> The full name is A B
+get('/user/:name/:last_name', function($name, $last_name){
+  echo "The full name is: $name $last_name ";
   exit();
 });
 
-// ##############################
-get('/xss', function(){
-  require_once(__DIR__.'/views/view_xss.php');
+// Dynamic GET. Example with 2 variables with static
+// In the URL -> http://localhost/product/shoes/color/blue
+// The output -> The product is shoes with color blue
+get('/product/:type/color/:color', function($type, $color){
+  echo "The product is $type with color $color ";
   exit();
 });
 
-// ##############################
-post('/check_page_view/:page_name', function($page_name){
-  echo $page_name;
-  $random = $page_name.'_'.bin2hex(random_bytes(16));
-  file_put_contents(__DIR__."/page_views/$random", "");
+// Dynamic GET. Example with 1 variable and 1 query string
+// In the URL -> http://localhost/item/car?price=10
+// The output -> The item name is car ane the price is 10
+get('/item/:name', function($name){
+  echo "The item name is $name ane the price is {$_GET['price']}";
   exit();
 });
 
-// ##############################
+// Dynamic GET. Example with 2 variables and 2 query strings
+// In the URL -> http://localhost/item/Table/20?weight=10&height=30
+// The output -> Name: Table Price: 20 Weight: 10 Height: 30
+get('/item/:name/:price', function($name, $price){
+  echo "Name: $name Price: $price Weight: {$_GET['weight']} Height: {$_GET['height']}";
+  exit();
+});
+
+// ##################################################
+// ##################################################
+// ##################################################
+// For post the exact same rules as get applied.
+// Look at the examples above.
+
+// Static POST
+// In the form  -> <input type="text" name="user_name" value="John">
+// The output   -> User created with name: John
+post('/users', function(){
+  echo "User created with name: {$_POST['user_name']}";
+  exit();
+});
+
+// ##################################################
+// ##################################################
+// ##################################################
+// any can be used for GETs or POSTs
+
+// For GET or POST
 any('/404', function(){
-  require_once(__DIR__.'/views/view_404.php');
+  echo 'Not found';
   exit();
 });
-
