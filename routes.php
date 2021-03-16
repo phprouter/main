d2407f986d387330416a34c8c884ee9d0862b2a5
phprouter <80062791+phprouter@users.noreply.github.com>
2021-03-16 18:14:35 +0100

    Update routes.php
    removed exit() since the router.php takes care of it now

--- routes.php
+++ routes.php
@@ -11,7 +11,6 @@ require_once(__DIR__.'/router.php');
 // The output -> Index
 get('/', function(){
   echo 'Index';
-  exit();
 });
 
 // Dynamic GET. Example with 1 variable
@@ -19,7 +18,6 @@ get('/', function(){
 // The output -> The id passed is: 111
 get('/user/:id', function($id){
   echo "The id passed is: $id ";
-  exit();
 });
 
 // Dynamic GET. Example with 2 variables
@@ -27,15 +25,13 @@ get('/user/:id', function($id){
 // The output -> The full name is A B
 get('/user/:name/:last_name', function($name, $last_name){
   echo "The full name is: $name $last_name ";
-  exit();
 });
 
 // Dynamic GET. Example with 2 variables with static
 // In the URL -> http://localhost/product/shoes/color/blue
 // The output -> The product is shoes with color blue
 get('/product/:type/color/:color', function($type, $color){
-  echo "The product is $type with color $color ";
-  exit();
+  echo "The product is $type with color $color";
 });
 
 // Dynamic GET. Example with 1 variable and 1 query string
@@ -43,7 +39,6 @@ get('/product/:type/color/:color', function($type, $color){
 // The output -> The item name is car ane the price is 10
 get('/item/:name', function($name){
   echo "The item name is $name ane the price is {$_GET['price']}";
-  exit();
 });
 
 // Dynamic GET. Example with 2 variables and 2 query strings
@@ -51,7 +46,6 @@ get('/item/:name', function($name){
 // The output -> Name: Table Price: 20 Weight: 10 Height: 30
 get('/item/:name/:price', function($name, $price){
   echo "Name: $name Price: $price Weight: {$_GET['weight']} Height: {$_GET['height']}";
-  exit();
 });
 
 // ##################################################
@@ -65,7 +59,6 @@ get('/item/:name/:price', function($name, $price){
 // The output   -> User created with name: John
 post('/users', function(){
   echo "User created with name: {$_POST['user_name']}";
-  exit();
 });
 
 // ##################################################
@@ -76,5 +69,4 @@ post('/users', function(){
 // For GET or POST
 any('/404', function(){
   echo 'Not found';
-  exit();
 });
