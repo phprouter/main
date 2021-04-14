dbcc8495585d18def10d6b6b389e72ec4e8ba494
phprouter <80062791+phprouter@users.noreply.github.com>
2021-04-14 18:27:41 +0200

    Update router.php
    UPDATED: Easier routes without callback

--- router.php
+++ router.php
@@ -1,19 +1,19 @@
 <?php
 
-function get($route, $function_to_call){
-  if( $_SERVER['REQUEST_METHOD'] == 'GET' ){ route($route, $function_to_call); }  
+function get($route, $path_to_include){
+  if( $_SERVER['REQUEST_METHOD'] == 'GET' ){ route($route, $path_to_include); }  
 }
-function post($route, $function_to_call){
-  if( $_SERVER['REQUEST_METHOD'] == 'POST' ){ route($route, $function_to_call); }    
+function post($route, $path_to_include){
+  if( $_SERVER['REQUEST_METHOD'] == 'POST' ){ route($route, $path_to_include); }    
 }
-function delete($route, $function_to_call){
-  if( $_SERVER['REQUEST_METHOD'] == 'DELETE' ){ route($route, $function_to_call); }    
+function delete($route, $path_to_include){
+  if( $_SERVER['REQUEST_METHOD'] == 'DELETE' ){ route($route, $path_to_include); }    
 }
-function any($route, $function_to_call){ route($route, $function_to_call); }
-function route($route, $function_to_call){
+function any($route, $path_to_include){ route($route, $path_to_include); }
+function route($route, $path_to_include){
   if($route == "/404"){
     http_response_code(404);
-    call_user_func_array($function_to_call, []);
+    include_once("{$_SERVER['DOCUMENT_ROOT']}/$path_to_include");
     exit();
   }  
   $request_url = filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_URL);
@@ -24,22 +24,23 @@ function route($route, $function_to_call){
   array_shift($route_parts);
   array_shift($request_url_parts);
   if( $route_parts[0] == '' && count($request_url_parts) == 0 ){
-    call_user_func_array($function_to_call, []);
+    include_once("{$_SERVER['DOCUMENT_ROOT']}/$path_to_include");
     exit();
   }
   if( count($route_parts) != count($request_url_parts) ){ return; }  
   $parameters = [];
   for( $i = 0; $i < count($route_parts); $i++ ){
     $route_part = $route_parts[$i];
-    if( preg_match("/^[:]/", $route_part) ){
-      $route_part = ltrim($route_part, ':');
+    if( preg_match("/^[$]/", $route_part) ){
+      $route_part = ltrim($route_part, '$');
       array_push($parameters, $request_url_parts[$i]);
+      $$route_part=$request_url_parts[$i];
     }
     else if( $route_parts[$i] != $request_url_parts[$i] ){
       return;
     } 
   }
-  call_user_func_array($function_to_call, $parameters);
+  include_once("{$_SERVER['DOCUMENT_ROOT']}/$path_to_include");
   exit();
 }
 function out($text){echo htmlspecialchars($text);}
