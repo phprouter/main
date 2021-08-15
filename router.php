305f8424e2f1a00c02d9dc14b2fb9098e79e4fdc
phprouter <80062791+phprouter@users.noreply.github.com>
2021-08-15 10:33:40 +0200

    Update $i to $__i__
    The variable $i in the loop caused an issue if the user declared the dynamic route with the same variable name. Now $i is changed to $__i__

--- router.php
+++ router.php
@@ -37,14 +37,14 @@ function route($route, $path_to_include){
   }
   if( count($route_parts) != count($request_url_parts) ){ return; }  
   $parameters = [];
-  for( $i = 0; $i < count($route_parts); $i++ ){
-    $route_part = $route_parts[$i];
+  for( $__i__ = 0; $__i__ < count($route_parts); $__i__++ ){
+    $route_part = $route_parts[$__i__];
     if( preg_match("/^[$]/", $route_part) ){
       $route_part = ltrim($route_part, '$');
-      array_push($parameters, $request_url_parts[$i]);
-      $$route_part=$request_url_parts[$i];
+      array_push($parameters, $request_url_parts[$__i__]);
+      $$route_part=$request_url_parts[$__i__];
     }
-    else if( $route_parts[$i] != $request_url_parts[$i] ){
+    else if( $route_parts[$__i__] != $request_url_parts[$__i__] ){
       return;
     } 
   }
