f5ab8276490ba8b965068a7f2efdd07d27d7f295
phprouter <80062791+phprouter@users.noreply.github.com>
2022-05-16 11:21:25 +0200

    Update router.php
    Added functionality for callbacks. A user can define a function in the route. This allows the code to execute a function and also include a normal php page

--- router.php
+++ router.php
@@ -19,6 +19,13 @@ function delete($route, $path_to_include){
 }
 function any($route, $path_to_include){ route($route, $path_to_include); }
 function route($route, $path_to_include){
+  
+  // Callback function
+  if( is_callable($path_to_include) ){
+    call_user_func($path_to_include);
+    exit();
+  }  
+  
   $ROOT = $_SERVER['DOCUMENT_ROOT'];
   if($route == "/404"){
     include_once("$ROOT/$path_to_include");
