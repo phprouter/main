b33fa1e91c250df430304348690021f9fbb719e1
phprouter <80062791+phprouter@users.noreply.github.com>
2022-10-26 21:01:22 +0200

    Create routes.php
    Added routes for callbacks with arguments and changed to use __DIR__

--- routes.php
+++ routes.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once("{$_SERVER['DOCUMENT_ROOT']}/router.php");
+require_once __DIR__.'/router.php';
 
 // ##################################################
 // ##################################################
@@ -9,28 +9,42 @@ require_once("{$_SERVER['DOCUMENT_ROOT']}/router.php");
 // Static GET
 // In the URL -> http://localhost
 // The output -> Index
-get('/', 'index.php');
+get('/', 'views/index.php');
 
 // Dynamic GET. Example with 1 variable
 // The $id will be available in user.php
-get('/user/$id', 'user.php');
+get('/user/$id', 'views/user');
 
 // Dynamic GET. Example with 2 variables
-// The $name will be available in user.php
-// The $last_name will be available in user.php
-get('/user/$name/$last_name', 'user.php');
+// The $name will be available in full_name.php
+// The $last_name will be available in full_name.php
+// In the browser point to: localhost/user/X/Y
+get('/user/$name/$last_name', 'views/full_name.php');
 
 // Dynamic GET. Example with 2 variables with static
 // In the URL -> http://localhost/product/shoes/color/blue
 // The $type will be available in product.php
 // The $color will be available in product.php
-get('/product/$type/color/:color', 'product.php');
+get('/product/$type/color/$color', 'product.php');
 
-// Dynamic GET. Example with 1 variable and 1 query string
-// In the URL -> http://localhost/item/car?price=10
-// The $name will be available in items.php which is inside the views folder
-get('/item/$name', 'views/items.php');
+// A route with a callback
+get('/callback', function(){
+  echo 'Callback executed';
+});
 
+// A route with a callback passing a variable
+// To run this route, in the browser type:
+// http://localhost/user/A
+get('/callback/$name', function($name){
+  echo "Callback executed. The name is $name";
+});
+
+// A route with a callback passing 2 variables
+// To run this route, in the browser type:
+// http://localhost/callback/A/B
+get('/callback/$name/$last_name', function($name, $last_name){
+  echo "Callback executed. The full name is $name $last_name";
+});
 
 // ##################################################
 // ##################################################
